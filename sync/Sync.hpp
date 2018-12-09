6604f431f922e15c9ab9e4b4e8d1d3d11b1e9ffb
ReQezeR <mp62639@o2.pl>
2018-12-09 17:41:58 +0100

    Dodałem deklaracje zmiennych i metod.

--- sync/Sync.hpp
+++ sync/Sync.hpp
@@ -1,8 +1,27 @@
 #pragma once
 class Sync
 {
+private:
+	bool lock;
+	bool cond;
+	std::list<PCB*>LockProcessQueue;//Kolejka procesow oczekujacych na zamku
+	std::list<PCB*>CondProcessQueue;//Kolejka procesow oczekujacych na zmiennej warunkowej
 public:
 	Sync();
 	~Sync();
+	//Metody do obslugi zamkow :
+	void acquire(PCB*);
+	void release(PCB*);
+	int  getLockID();
+	bool getLock();
+	std::list<PCB*>getLPQ();
+
+	//Metody do obslugi zmiennych warunkowych :
+	void wait(PCB*);
+	void signal();
+	void broadcast();
+	int  getCondID();
+	bool getCond();
+	std::list<PCB*>getCPQ();
 };
 

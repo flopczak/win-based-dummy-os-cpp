5b53aa4468e46daa81e7708a6ed2cf410f2e0a55
ReQezeR <mp62639@o2.pl>
2018-12-17 17:17:52 +0100

    Dodałem wstępną implementacje metod dla zamków + aktualizacja nazw zmiennych.

--- sync/Sync.cpp
+++ sync/Sync.cpp
@@ -1,12 +1,62 @@
 #include "Sync.hpp"
-
+#include "Procesor.hpp"
 
 
 Sync::Sync()
 {
+	this->lock=false;
+	this->cond=false;
 }
 
 
 Sync::~Sync()
 {
+}
+
+void acquire(Procesor::Process*tempProcess)
+{
+	if (this->lock == true) 
+	{
+		tempProcess->setProcessStatus("OCZEKUJACY"); // Process do stanu waiting. 
+		LockProcessQueue.push_back(tempProcess);
+	}
+	else if (this->lock == false) 
+	{
+		this->lock = true;
+		this->currentLockProcess->setPID(tempProcess->pID);
+		this->currentLockProcess = tempProcess;
+	}
+
+}
+
+void release(Procesor::Process*tempProcess)
+{
+	if (this->getLockID() == tempProcess->pID)	
+	{
+		if (LockProcessQueue.empty() == true)
+		{ 
+			this->lock = false; 
+		}
+
+		else 
+		{
+			this->currentLockProcess = LockProcessQueue.front();
+			LockProcessQueue.pop_front();
+			this->currentLockProcess->setProcessStatus("GOTOWY") //Process do stanu ready.
+			this->currentLockProcess->setPID(tempProcess->getPID()); // setPID() potrzebne!! @Blazej
+		}
+	}
+	else this->synchProces.setProcessStatus("ZAKONCZONY"); //Process do stanu terminated.
+	
+
+}
+
+int getLockID()
+{
+	return this->currentLockProcess->getPID(); // getPID() potrzebne!! @Blazej
+}
+
+bool getLock()
+{
+	return this->lock;
 }
--- sync/Sync.hpp
+++ sync/Sync.hpp
@@ -1,28 +1,32 @@
 #pragma once
+#include "Procesor.hpp"
+
 class Sync
 {
 private:
+	Procesor::Process*currentLockProcess;
+	Procesor::Process*currentCondProcess;
 	bool lock;
 	bool cond;
-	std::list<PCB*>LockProcessQueue;//Kolejka procesow oczekujacych na zamku
-	std::list<PCB*>CondProcessQueue;//Kolejka procesow oczekujacych na zmiennej warunkowej
+	std::list<Procesor::Process*>LockProcessQueue;//Kolejka procesow oczekujacych na zamku
+	std::list<Procesor::Process*>CondProcessQueue;//Kolejka procesow oczekujacych na zmiennej warunkowej
 public:
 	Sync();
 	~Sync();
 	//Metody do obslugi zamkow :
-	void acquire(PCB*);
-	void release(PCB*);
+	void acquire(Procesor::Process*);
+	void release(Procesor::Process*);
 	int  getLockID();
 	bool getLock();
-	std::list<PCB*>getLPQ();
+	std::list<Procesor::Process*>getLPQ();
 
 	//Metody do obslugi zmiennych warunkowych :
-	void wait(PCB*);
+	void wait(Procesor::Process*);
 	void signal();
 	void broadcast();
 	int  getCondID();
 	bool getCond();
-	std::list<PCB*>getCPQ();
+	std::list<Procesor::Process*>getCPQ();
 
 };
 

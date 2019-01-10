daf2e8a4402aaa45045876a2f4491f2ea4e0646b
Błażej Darul <darullef4@gmail.com>
2019-01-10 13:56:44 +0100

    added more useful functions

--- process management/Process.cpp
+++ process management/Process.cpp
@@ -17,13 +17,13 @@ Process::Process()
 	this->CX = 0;
 }
 
-Process::Process(string na, int pr, int in)
+Process::Process(string na, int pr)
 {
 	this->PID = rand() % 9999 + 1000;
 	this->process_name = na;
 	this->process_status = NOWY;
 	this->process_priority = pr;
-	this->program_instructions = in;
+	this->program_instructions = 0;
 	this->AX = 0;
 	this->BX = 0;
 	this->CX = 0;
@@ -49,9 +49,9 @@ void Process::setInstructions(int in)
 	this->program_instructions = in;
 }
 
-void Process::setProcessStatus(string st)
+void Process::setProcessStatus(status)
 {
-	this->process_status = st;
+	this->process_status = NOWY, AKTYWNY, GOTOWY, OCZEKUJACY, ZAKONCZONY;
 }
 
 void Process::display() 
@@ -60,16 +60,19 @@ void Process::display()
 	cout << "Nazwa procesu: " << this->process_name << endl;
 	cout << "Status procesu: " << this->process_status << endl;
 	cout << "Priorytet: " << this->process_priority << endl;
+	cout << "AX: " << this->AX << endl;
+	cout << "BX: " << this->BX << endl;
+	cout << "CX: " << this->CX << endl;
 }
 
-void Process::findAndDisplayProcess(string s) 
+void Process::findAndDisplayProcess(string s)
 {
 	bool czy = false;
-	for (int i = 0; i < Process_List::PrcList.size(); i++)
+	for (auto it : Process_List::PrcList)
 	{
-		//if (s == Process_List::PrcList[i].process_name)
+		if (it.process_name == s)
 		{
-			//Process_List::PrcList[i].display();
+			it.display();
 			czy = true;
 		}
 	}
@@ -79,10 +82,51 @@ void Process::findAndDisplayProcess(string s)
 	}
 }
 
-void Process::addProcess(Process a) 
+void Process::displayHelper()
+{
+	cout << "PID: " << this->PID << endl;
+	cout << "Nazwa procesu: " << this->process_name << endl;
+	cout << "Status procesu: " << this->process_status << endl;
+	cout << "Priorytet: " << this->process_priority << endl;
+}
+
+void Process::displayAll()
+{
+	for (auto it : Process_List::PrcList)
+	{
+		it.displayHelper();
+		cout << endl;
+	}
+}
+
+void Process::addProcess(Process a)
 {
 	Process_List::PrcList.push_front(a);
 }
+
+void Process::removeProcess() 
+{
+	for (auto it : Process_List::PrcList)
+	{
+		if (it.process_status == ZAKONCZONY)
+		{
+			Process_List::PrcList.remove(it);
+		}
+	}
+}
+
+void Process::terminateProcess(string s)
+{
+	for (auto it : Process_List::PrcList)
+	{
+		if (it.process_name == s)
+		{
+			Process_List::PrcList.remove(it);
+		}
+	}
+}
+
+//---------------------------------------------------------------------//
 
 void Process::UstTabStronic(STRON* newpageTable)
 {
--- process management/Process.hpp
+++ process management/Process.hpp
@@ -11,36 +11,40 @@ struct STRON
 	bool wPam;
 };
 
-class Process							//PCB
+class Process											//PCB
 {
 public:
-	string process_name;				//nazwa procesu
+	string process_name;								//nazwa procesu
 
 private:
-	STRON* pageTable;	//Przemkowe RAM'y
+	STRON* pageTable;									//Przemkowe RAM'y
 	int pageTableSize;
 
-	static int PID;	//indywidualny identyifkator
-	string process_status;	//status procesu: nowy, aktywny, gotowy, oczekuj¹cy, zakoñczony
-	int program_instructions;	//instrukcje asemblerowskie
-	int AX;	//rejestry
+	static int PID;										//indywidualny identyifkator
+	status process_status;								//status procesu: nowy, aktywny, gotowy, oczekuj¹cy, zakoñczony
+	int program_instructions;							//instrukcje asemblerowskie
+	int AX;												//rejestry
 	int BX;
 	int CX;
-	int process_priority;	//priorytet
+	int process_priority;								//priorytet
 	
 	
-	Process();	//konstruktor domyœlny
-	Process(string n, int p, int i);	//konstruktor
-	~Process();	//dekonstruktor
+	Process();											//konstruktor domyœlny
+	Process(string n, int p);							//konstruktor
+	~Process();											//dekonstruktor
 	int errorCode();
-	void setInstructions(int in);	//przekazywanie instrukcji asemblera do procesu
-	void setProcessStatus(string st);	//ustawianie statusu procesu
-	void setPriority(int n);	//ustawianie priorytetu
-	void display();	//wyœwietlanie procesu		
-	void findAndDisplayProcess(string s);	//szukanie procesu po nazwie w kolejce i wyœwietlanie
-	void addProcess(Process a);	//dodawanie procesu do kolejki
+	void setInstructions(int in);						//przekazywanie instrukcji asemblera do procesu
+	void setProcessStatus(status);						//ustawianie statusu procesu
+	void setPriority(int n);							//ustawianie priorytetu
+	void display();										//wyœwietlanie szczegó³owe jednego procesu
+	void displayAll();									//wyœwietlanie wszystkich procesow
+	void displayHelper();								//funkcja pomocnicza dla funckcji displayAll()
+	void findAndDisplayProcess(string s);				//szukanie procesu po nazwie w kolejce i wyœwietlanie
+	void addProcess(Process a);							//dodawanie procesu do kolejki
+	void removeProcess();								//usuwanie zakonczonego procesu
+	void terminateProcess(string s);						//koñczenie procesu przez u¿ytkownika
 	
-	void UstTabStronic(STRON* newpageTable);	//Przemkowe RAM'y
+	void UstTabStronic(STRON* newpageTable);			//Przemkowe RAM'y
 	STRON* PobTabStronic();
 	void UstWielTabStronic(int num);
 	int PobWielTabStronic();

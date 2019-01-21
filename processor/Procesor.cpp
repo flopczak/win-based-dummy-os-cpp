9311b4f0d1b75ec66f1beb649159dd9860b3722e
flopczak <jakubflorczak27@gmail.com>
2019-01-21 12:02:49 +0100

    bez całego ageingu

--- processor/Procesor.cpp
+++ processor/Procesor.cpp
@@ -1,10 +1,14 @@
 #include "Procesor.hpp"
 #include <iostream>
 
-bool debug = true;
 
-Procesor::Procesor() //maska inaczej?
+
+bool work = true;
+
+Procesor::Procesor()
 {
+	w_counter = 1;
+	new_process = false;
 	mask[0] = true;
 	for (int i = 1; i < 8; i++)
 	{
@@ -17,48 +21,105 @@ Procesor::~Procesor()
 {
 }
 
-void Procesor::add(Process&ready) // przemysl czy ageing musi by� oddzielny i czy chcesz doda� cos do PCB
+void Procesor::check(Process&ready, Procesor&p)
+{
+	if (ready.process_priority > p.running.process_priority)
+	{
+		if (p.running.process_priority != 0)
+		{
+			p.running.process_status = GOTOWY;
+			int it = p.running.process_priority;
+			p.main_queue[it].push_back(p.running);
+			p.running = ready;
+			p.running.process_status = AKTYWNY;
+			p.mask[it] = true;
+			p.new_process = false;
+		}
+	}
+	else
+	{
+		p.new_process = false;
+
+		std::cout << "nowy proces ma taki sam priorytet lub mniejszy kontynuje prace " << endl;
+	}
+
+}
+
+void Procesor::add(Process&ready, Procesor&p)
 {
 	int it = ready.process_priority;
 	main_queue[it].push_back(ready);
 	mask[it] = true;
+	new_process = true;
+	check(ready, p);
+
 }
 
 
 
-void Procesor::find_and_run(Procesor& p) //przemysl jak sprawdzac czy pojawil sie nowy proces
+
+
+
+void Procesor::find_and_run(Procesor& p) //sprawdzanie co każdą iterację pentli w main
 {
-	int it = 8;
-	while (it < 0)
+	int it = 7;
+	while (it > 0)
 	{
 		if (p.mask[it] == true)
 		{
-				Process temp = main_queue[it].front;
-				temp.process_status = AKTYWNY;
-				main_queue[it].pop_front;
-				if (main_queue[it].empty())
-				{
-					p.mask[it] = false;
-				}
-				else
-				{
-					p.mask[it] = true;
-				}
+
+			if (main_queue[it].empty())
+			{
+				p.mask[it] = false;
+			}
+			else
+			{
+				p.mask[it] = true;
+			}
+			break;
 		}
 		else if (it == 0)
 		{
-			Process temp = main_queue[it].front;
-			temp.process_status = AKTYWNY;
+			running = main_queue[it].front();
+			cout << "running DUMMY" << endl;
 		}
-		else continue;
+		else
+		{
 
-		it--;
+			it--;
+		}
 	}
+
+	running = main_queue[it].front();
+	running.process_status = AKTYWNY;
+	cout << "running: " << &running.process_name << endl;
+	main_queue[it].pop_front();
+	if (main_queue[it].empty())
+	{
+		mask[it] = false;
+	}
+	cin.get();
+
 }
 
 
 
-void Procesor::age(Procesor& p)
+void Procesor::age(Process& p)
 {
-
+	if (p.process_priority > 0 && p.process_priority < 2)
+	{
+		p.process_priority = 2;
+	}
+	else if (p.process_priority > 0 && p.process_priority > 1 && p.process_priority < 4)
+	{
+		p.process_priority = 4;
+	}
+	else if (p.process_priority > 0 && p.process_priority > 3 && p.process_priority < 6)
+	{
+		p.process_priority = 6;
+	}
+	else if (p.process_priority > 0 && p.process_priority == 6)
+	{
+		p.process_priority = 7;
+	}
 }

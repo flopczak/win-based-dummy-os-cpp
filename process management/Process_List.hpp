6b2ad3af8a33ec6c244a5c5d92ce739b2df1e633
Błażej Darul <darullef4@gmail.com>
2019-01-21 12:55:04 +0100

    funkcje co do wyciągania procesów z listy sa w Process_List

--- process management/Process_List.hpp
+++ process management/Process_List.hpp
@@ -8,5 +8,11 @@ class Process_List
 {
 public:
 	static list<Process>PrcList;
+	list<Process> getReady();
+	void terminateProcess(string s);
+	void removeProcess();
+	void createProcess(string na);
+	void createProcess(string na, int pr);
+	void findAndDisplayProcess(string s);
+	void displayAll();
 };
-

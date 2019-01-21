c785b4a4ddd12238d9973ad967c3308323bb9a34
Bartłomiej Olszanowski <baloo@int.pl>
2019-01-21 15:09:29 +0100

    Integration `processor` with `processes`

--- process management/Process_List.hpp
+++ process management/Process_List.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "Process.hpp"
+#include "../processor/Procesor.hpp"
 #include <deque>
 #include <list>
 using namespace std;
@@ -8,6 +9,7 @@ class Process_List
 {
 public:
 	static list<Process>PrcList;
+	Procesor procesor = new Procesor(this);
 	list<Process> getReady();
 	void terminateProcess(string s);
 	void removeProcess();

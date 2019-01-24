a571d122306821c5a6995c271993fa2b911a0a52
DalduK <dalduk14@gmail.com>
2019-01-24 02:25:53 +0100

    Zmiana pcb na PID

--- memory/Memory.hpp
+++ memory/Memory.hpp
@@ -1,7 +1,6 @@
 #pragma once
 #ifndef Memory_H
 #define Memory_H
-#include "../process management/Process.hpp"
 #include <iostream>
 #include <list>
 #include <queue>
@@ -23,13 +22,19 @@ enum class CharTable : unsigned char
 };
 
 class Process;
-struct STRON;
+
+struct STRON
+{
+	int RamaZajeta;
+	bool wPam;
+};
+
 struct FIFO_entry {
-	Process * pcb;
+	int PID;
 	int pageNumber;
 
-	FIFO_entry(Process * pcb, int pageNumber) {
-		this->pcb = pcb;
+	FIFO_entry(int PID, int pageNumber) {
+		this->PID = PID;
 		this->pageNumber = pageNumber;
 	}
 };
@@ -37,72 +42,74 @@ struct FIFO_entry {
 class Memory
 {
 private:
-	enum class BledyPam {
-		OUT_OF_RANGE = 9,
-		OUT_OF_MEM = 10
-	};
 	//---RAM---//
 	char * RAM;
 	const int WIE_PAM = 256;
 	const int WIE_RAM = 16;
 	const int ILOSC_RAM = WIE_PAM / WIE_RAM;
 	//---Obliczenia zwiazane z ramem i plikiem wym---//
-	int ObliczOffset(int addr1);
-	int ObliczNumerStrony(int addr1);
-	int ObliczAdresFizyczny(Process * pcb, int addr1);
-	int ObliczTabliceStronic(int siBytes);
+	const int ObliczOffset(int addr1);
+	const int ObliczNumerStrony(int addr1);
+	const int ObliczAdresFizyczny(int PID, int addr1);
+	const int ObliczTabliceStronic(int siBytes);
 	//---Zapewnienie wolnej ramy Procesowi---//
-	void ZapewnijWolnaRame();//=Zapewnienie ¿e rama jest wolna
+	const void ZapewnijWolnaRame();//=Zapewnienie ¿e rama jest wolna
 	//---Odczyt Pamieci---//
-	char OdczytajZPamieci(Process * pcb, int addr1);
+	const char OdczytajZPamieci(int PID, int addr1);
 	//---Zapisanie danych w PAMRam---//
-	void ZapiszWPamieci(Process * pcb, int addr1, char element);
+	const void ZapiszWPamieci(int PID, int addr1, char element);
 	//---Zapewnienie miejsca dla Strony w pamieci ram---//
-	void ZapewnijStroneWPamieci(Process * pcb, int logicalAddress);
+	const void ZapewnijStroneWPamieci(int PID, int logicalAddress);
 	//---Pobranie Ramy z pamieci dla procesu---//
-	int PobierzWolRamePamieci();
+	const int PobierzWolRamePamieci();
 	//---PLIK WYMIANY---//
+	int pageTableSize;
 	char * PlikWymiany;
 	const int ILOSC_RAM_P_WYMIANY = 1024;
 	const int WIE_P_WYMIANY = ILOSC_RAM_P_WYMIANY * WIE_RAM;
 	//---Zapis do pliku wymiany bez pam ram---//
-	void ZapiszDoPlikuWym(int nr, string Content);
+	const void ZapiszDoPlikuWym(int nr, string Content);
 	//---Pobieranie wolnej ramki z p. Wymiany---//
-	int PobierzWolnaRamePWym();
+	const int PobierzWolnaRamePWym();
 	//---Wyczyszczenie pustej ramki pliku wymiany---//
-	void WyczyscRamPlikuWym(int frameNumber);
+	const void WyczyscRamPlikuWym(int frameNumber);
 	list<int> WolneRamki;
 	list<int> WolneRamkiPlikuWymiany;
 	list<FIFO_entry> FIFO;
 	//---Przenoszenie z pliku wymiany do ramu i w druga strone---//
-	void PrzeStroPWymDoPam(Process * pcb, int pageNr);
-	void PrzeStroPamDoPWym(Process * pcb, int pageNr);
-	void PrzeZawPamDoPWym(int MemoryFrame, int SwapFileFrame);
-	void PrzeZawPWymDoPam(int SwapFileFrame, int MemoryFrame);
+	const void PrzeStroPWymDoPam(int PID, int pageNr);
+	const void PrzeStroPamDoPWym(int PID, int pageNr);
+	const void PrzeZawPamDoPWym(int MemoryFrame, int SwapFileFrame);
+	const void PrzeZawPWymDoPam(int SwapFileFrame, int MemoryFrame);
 	//---Wpis do Fifo---//
-	void DodajDoFifo(FIFO_entry entry);
-	bool BrakMieWPowAdr(Process * pcb, int logicalAddress);
+	const void DodajDoFifo(FIFO_entry entry);
+	const bool BrakMieWPowAdr(int PID, int logicalAddress);
 public:
-	void UtworzProgram(Process*pcb,string path);
+	STRON* pageTable;
+	const void UtworzProgram(int PID, string path);
 	//-----Pamiec-----//
-	void PrzydzialPamieci(Process * pcb, string proces, int size);
-	void zwolnieniePamieci(Process * pcb);
-	void WypiszZasobPamieci(int nrToPrint = 0);
-	void WpiszZasobPamDoPWym(int nr, string zasobPam);
+	const void PrzydzialPamieci(int PID, string proces, int size);
+	const void zwolnieniePamieci(int PID);
+	const void WypiszZasobPamieci(int nrToPrint = 0);
+	const void WpiszZasobPamDoPWym(int nr, string zasobPam);
 	//----Sprawdzanie Powierzchni Adresowej----//
-	bool CzyAdrWPowAdresss(Process * pcb, int AddrLog);
-	bool CzyZasiegAdrWPowAdres(Process * pcb, int AddrLog, int range);
+	const bool CzyAdrWPowAdresss(int AddrLog);
+	const bool CzyZasiegAdrWPowAdres(int AddrLog, int range);
 	//----Drukowanie----//
-	void WydrukujProcesy(Process * pcb, bool wRamie);
-	void WydrukujFIFO();
-	void WydrukujTabliceStronic(Process * pcb);
-	void WydrukujRame(int RamaNr, int StronaNr = -1);
-	void WydrukujRamePWym(int RamaNr, int StronaNr = -1);
+	const void WydrukujProcesy(int PID, bool wRamie);
+	const void WydrukujFIFO();
+	const void WydrukujTabliceStronic(int PID);
+	const void WydrukujRame(int RamaNr, int StronaNr = -1);
+	const void WydrukujRamePWym(int RamaNr, int StronaNr = -1);
 	//----Odczytywanie i zapisywanie stringow----//
 	//(potrzebne do odczytu procesow//
-	void zapiszString(Process*pcb, int Addr1, string content);
-	string odczytajString(Process * pcb, int Addr1);
+	const void zapiszString(int PID, int Addr1, string content);
+	const string odczytajString(int PID, int Addr1);
 	//konstruktor//
+	void UstTabStronic(STRON* newpageTable);			//Przemkowe RAM'y
+	STRON* PobTabStronic();
+	void UstWielTabStronic(int num);
+	int PobWielTabStronic();
 	Memory();
 	~Memory();
 };
